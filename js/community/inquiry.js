5f0d14289e7b1552137c9a705e0828a76e6c41ed
taemin1997 <taemin9709@gmail.com>
2024-05-13 15:37:55 +0900

    모달 추가중

--- js/community/inquiry.js
+++ js/community/inquiry.js
@@ -4,5 +4,11 @@
 // let contentCount = document.querySelectorAll('.inquiry-contents').length;
 // let endPage = Math.ceil((contentCount / showContent));
 
-const inquiryModal = document.querySelectorAll('.inquiry-modal');
+const inquiryModal = document.querySelector('.inquiry-modal');
+const modalBtn = document.querySelector('.inquiry-button button[type="button"]');
 console.log(inquiryModal);
+console.log(modalBtn);
+
+modalBtn.addEventListener('click', () => {
+  inquiryModal.style.display = 'block';
+})
