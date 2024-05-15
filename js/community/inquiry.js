f98eb0795c4085b511a34bcbe893ce3ce4388269
taemin1997 <taemin9709@gmail.com>
2024-05-15 20:43:40 +0900

    컴퓨터 바꾸기용

--- js/community/inquiry.js
+++ js/community/inquiry.js
@@ -41,3 +41,85 @@ answerModalBtn.forEach((btn) => {
     inquiryAnswerModal.style.display = 'block';
   })
 });
+
+
+const modalTitle = document.querySelector('.inquiry-modal-title input[type="text"]');
+const modalContent = document.querySelector('.inquiry-modal-content textarea');
+const modalSubmit = document.querySelector('.inquiry-modal-btns button[type="submit"]');
+
+// console.log(modatContent);
+// console.log(modalTitle);
+console.log(modalSubmit);
+
+modalSubmit.addEventListener('click', () =>{
+  if(modalContent.value === "" || modalTitle.value === ""){
+    alert('제목 또는 내용 입력');
+    inquiryModal.style.display = 'flex';
+    if(modalContent.value === ""){
+      modalContent.style.border = "none";
+      modalContent.style.outline = "1px solid red";
+
+      modalContent.addEventListener('keydown', () => {
+        modalContent.style.outline = "1px solid skyblue"; 
+      });
+    }
+    if(modalTitle.value === ""){
+      modalTitle.style.border = "none";
+      modalTitle.style.outline = "1px solid red";
+
+      modalTitle.addEventListener('keydown', () => {
+        modalTitle.style.outline = "1px solid skyblue";
+      })
+    }
+  }else{
+    let submit = confirm("등록하시겠습니까?");
+    if(submit){
+      alert("등록완료");
+      inquiryModal.style.display = 'none';
+    }else{
+      inquiryModal.style.display = 'flex';
+    }
+  }
+})
+
+const inquiryPrev = document.querySelector('.inquiry-prev');
+const inquiryNext = document.querySelector('.inquiry-next');
+const bannerImg = document.querySelectorAll('.inquriy-banner-img');
+let bannerWidth = 900;
+let currentIdx = 0;
+let imgLength = bannerImg.length;
+
+// console.log(inquiryNext);
+// console.log(inquiryPrev);
+// console.log(bannerImg);
+
+inquiryNext.addEventListener('click', () =>{
+  currentIdx++;
+  bannerImg.forEach((img) =>{
+    img.style.left = -(currentIdx * bannerWidth) + "px";
+    img.style.transition = "0.5s ease";
+    checkEnd();
+  })
+});
+inquiryPrev.addEventListener('click', () =>{
+  currentIdx--;
+  bannerImg.forEach((img) =>{
+    img.style.left = -(currentIdx * bannerWidth) + "px";
+    img.style.transition = "0.5s ease";
+    checkEnd();
+  })
+});
+
+function checkEnd(){
+  if(currentIdx <= 0){
+    inquiryPrev.style.display = 'none';
+  }else{
+    inquiryPrev.style.display = 'block';
+  }
+
+  if(currentIdx >= imgLength - 1){
+    inquiryNext.style.display = 'none';
+  }else{
+    inquiryNext.style.display = 'block';
+  }
+}
